4c47c7268483e7a870cd1d2ce0b34cb64aa0f08c
Airseai6 <1042536516@qq.com>
2019-12-24 07:15:41 +0800

    for instance Signed-off-by: Airseai6 <1042536516@qq.com>

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 #! python3
 # -*- coding:utf-8 -*-
+from MDSplus import *
 
 
-def main():
-    print("Hello Word!")
+t = Tree("my_tree", -1)
+node1 = t.getNode("NUM3")
+node2 = t.getNode("SUB2")
+print(node1)
+print(node2)
+d1 = node1.getData()
+d2 = node2.getNodeWild('***')
+print(d1)
+print(d2.getPath())
 
-
-if __name__ == '__main__':
-    main()
