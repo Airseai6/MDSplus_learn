25151e72dee9dcccce8ee73d8320eb01902ec344
littleStone <qidongkai@addom.xinaogroup.com>
2019-12-25 16:39:16 +0800

    Signed-off-by: littleStone <qidongkai@addom.xinaogroup.com>

--- test2.py
+++ test2.py
@@ -8,12 +8,30 @@ from MDSplus import *
 def main():
     t = Tree('exl50', -1)
     node1 = t.getNode("AI")
-    # node1 = t.getNode("FBC")
+    node2 = t.getNode("FBC")
     d1 = node1.getNodeWild('***')
+    d2 = node2.getNodeWild('***')
     print(len(d1.getPath()))
-    print(d1.getPath()[639])
+    print(x.getNodeName() for x in d1.getPath())
+    print(len(d2.getPath()))
+
+
+def get_nodes_name(shot):
+    subtrees = ["AI", "FBC"]
+    nodes_name_list = []
+    t = Tree("exl50", shot)
+    for subtree in subtrees:
+        nodes = t.getNode(subtree)
+        nodes_d = nodes.getDescendants()
+        nodes_name_list.extend(nodes_d)
+        pool = []
+        for node_name in nodes_name_list:
+            pool.append(node_name.getNodeName)
+    return pool
 
 
 if __name__ == '__main__':
     main()
+    print(len(get_nodes_name(-1)))
+    print(get_nodes_name(-1))
 
