73cccfed0abbc2b48cd8b94b42ff84205522560c
littleStone <1042536516@qq.com>
2019-12-31 13:53:45 +0800

    Signed-off-by: littleStone <1042536516@qq.com>

--- copyATree.py
+++ copyATree.py
@@ -12,10 +12,10 @@ def copy_tree(shot):
     pools = ["AI", "FBC"]
 
     for pool in pools:
-        nodes_list = []
+        # nodes_list = []
         nodes_array = tree_raw.getNode(pool).getDescendants()
-        nodes_list.extend(nodes_array)
-        pool_nodes = [node.getNodeName() for node in nodes_list]
+        # nodes_list.extend(nodes_array)
+        pool_nodes = [node.getNodeName() for node in nodes_array]
 
         tree_raw.setDefault(tree_raw.getNode(pool))
         tree_cop.setDefault(tree_cop.addNode(pool, "STRUCTURE"))
@@ -28,29 +28,11 @@ def copy_tree(shot):
                 print(str(shot) + ':' + node)
             except Exception as e:
                 pass
-        tree_raw.setDefault(tree_raw.getNode("\EXL50::TOP"))
-        tree_cop.setDefault(tree_cop.getNode("\EXL50_COPY::TOP"))
+        tree_raw.setDefault(tree_raw.getNode(r"\EXL50::TOP"))
+        tree_cop.setDefault(tree_cop.getNode(r"\EXL50_COPY::TOP"))
         # tree_raw.setDefault(node.getParent())
         # tree_cop.setDefault(node.getParent())
-
-
     tree_cop.write()
-
-
-# def copy_tree2(shot):
-#     tree_raw = Tree('exl50', shot)
-#     tree_cop = Tree('exl50_copy', shot, 'NEW')
-#     subtrees = ["AI", "FBC"]
-#
-#     for subtree in subtrees:
-#         nodes = tree_raw.getNode(subtree).getNodeWild('***')
-#         pool_nodes = nodes.getPath()
-#
-#         for node in pool_nodes:
-#             tree_cop.addNode(node)
-#             print(node)
-#
-#     tree_cop.write()
 
 
 def cal_time(fun):
@@ -64,11 +46,11 @@ def cal_time(fun):
 
 @cal_time
 def main():
-    copy_tree(-1)
     # copy_tree(-1)
-    # for i in range(3601, 3604):
-    #     print(i)
-    #     copy_tree(i)
+    # # copy_tree(-1)
+    for i in range(3604, 3606):
+        print(i)
+        copy_tree(i)
 
 
 if __name__ == '__main__':
